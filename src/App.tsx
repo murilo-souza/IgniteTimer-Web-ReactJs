61b4531bf0d1e3ffde6992bee77a92da14e13af5
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-12-14 15:33:34 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <h1>kiwnwbdfyuwb</h1>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
-  );
+  )
 }
