a732b7e01b2d262189ef8be5ab03d5832d4f0493
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-12-14 19:58:19 -0300

    update

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -47,6 +47,38 @@ export const HistoryList = styled.div`
       padding: 1rem;
       font-size: 0.875rem;
       line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
     }
+  }
+`
+
+const STATUS_COLOR = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLOR
+}
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 9999px;
+    background: ${(props) => props.theme[STATUS_COLOR[props.statusColor]]};
   }
 `
